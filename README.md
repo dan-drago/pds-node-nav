6aa89ae4a8d42791327ef40d5f303ed16263eb3b
dwd-umd <ddarg@umd.edu>
2019-07-28 18:49:01 -0400

    Init commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# PDS NODE NAVIGATOR
+
+Exploring threeJs-based design for a simple site to introduce and navigate between the PDS Nodes
---
+++ src/PdsNodeNav/PdsNodeNavSceneManager.ts
@@ -0,0 +1,29 @@
+import { SceneManagerBase, ISceneManager } from '../ThreeJsScaffold/scene.manager';
+import { MiscHelpers } from './PdsNodeNavEntities/Helpers';
+import { DirectionalLight } from './PdsNodeNavEntities/DirectionalLight';
+import { Square } from './PdsNodeNavEntities/Square';
+
+export class PdsNodeNavSceneManager extends SceneManagerBase implements ISceneManager {
+  constructor(container: HTMLElement) {
+    super(container);
+
+    // Style canvas
+    container.style.backgroundColor = 'rgba(0,0,0,1)';
+
+    // Create entities
+    this._sceneEntities = [
+      //
+      new DirectionalLight(this),
+      new MiscHelpers(this),
+      // new SimpleLight(this),
+      new Square(this, 1)
+    ];
+
+    // Set this as entities parent
+    this._sceneEntities.forEach(el => el.init());
+  }
+
+  updateCamera = (time: number) => {
+    //
+  };
+}

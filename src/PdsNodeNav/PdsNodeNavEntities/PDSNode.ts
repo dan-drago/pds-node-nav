113543134eeb925112c7838d064b74b732f77ebd
dwd-umd <ddarg@umd.edu>
2019-07-30 01:23:07 -0400

    Basic orbiting nodes

--- src/PdsNodeNav/PdsNodeNavEntities/PDSNode.ts
+++ src/PdsNodeNav/PdsNodeNavEntities/PDSNode.ts
@@ -1,24 +1,37 @@
 import * as THREE from 'three';
 import { ISceneEntity, SceneEntityBase } from '../../ThreeJsScaffold/scene.entity';
 import { ISceneManager } from '../../ThreeJsScaffold/scene.manager';
+import { PDSNodeNavSceneManager } from '../PDSNodeNavSceneManager';
 
 export class PDSNode extends SceneEntityBase implements ISceneEntity {
+  private _children: PDSNode[] = [];
+  private _PdsNodeNavSceneManager: PDSNodeNavSceneManager;
+
   constructor(
     parentSceneManager: ISceneManager,
-    private parentPDSNode: PDSNode | null,
-    private radius: number
+    private _parentPDSNode: PDSNode | null,
+    private _name: string,
+    private _radius: number
   ) {
     super(parentSceneManager);
+
+    this._PdsNodeNavSceneManager = parentSceneManager as any;
+
+    this._sceneEntityGroup.name = _name;
+
+    // Add this node to parent's children
+    if (!!_parentPDSNode) _parentPDSNode.addToChildren(this);
   }
 
   init = () => {
     // const material = new THREE.MeshPhongMaterial();
     const material = new THREE.MeshBasicMaterial({ color: 0xffff00 });
 
-    const geometry = new THREE.CylinderGeometry(5, 5, 20, 32);
+    const geometry = new THREE.CylinderGeometry(this._radius, this._radius, 0.001, 32);
     const cylinderMesh = new THREE.Mesh(geometry, material);
     cylinderMesh.position.set(0, 0, 0);
-
+    cylinderMesh.rotateX(Math.PI / 2);
+    cylinderMesh.name = this._name;
     this._sceneEntityGroup.add(cylinderMesh);
 
     // Finish
@@ -27,10 +40,39 @@ export class PDSNode extends SceneEntityBase implements ISceneEntity {
   };
 
   update = (time: number) => {
-    // this._sceneEntityGroup.children.forEach(el => {
-    //   el.rotation.x += 0.01;
-    //   el.rotation.y += 0.02;
-    //   el.position.set(10 * Math.cos(time), 0, 0);
-    // });
+    const t = time * 0.05;
+    let orbitalRadius = 0;
+    const rf = this._PdsNodeNavSceneManager.getRadiusShrinkFactor();
+    if (this._radius === rf) orbitalRadius = 1 / rf;
+    if (this._radius === rf * rf) orbitalRadius = 1 / rf;
+
+    orbitalRadius = this._radius * 6;
+
+    const parentPosition = this._parentPDSNode
+      ? this._parentPDSNode.getPosition()
+      : new THREE.Vector3();
+
+    const siblings: PDSNode[] = this._parentPDSNode ? this._parentPDSNode.getChildren() : [];
+    const childPosition = !!siblings ? siblings.indexOf(this) : 0;
+    const thetaOffset = !!childPosition ? childPosition / siblings.length : 0;
+    const x = orbitalRadius * Math.cos((Math.PI * t) / orbitalRadius + 2 * Math.PI * thetaOffset);
+    const y = orbitalRadius * Math.sin((Math.PI * t) / orbitalRadius + 2 * Math.PI * thetaOffset);
+    const newPosition = new THREE.Vector3(parentPosition.x + x, parentPosition.y + y, 0);
+    this._sceneEntityGroup.position.set(newPosition.x, newPosition.y, newPosition.z);
+  };
+
+  getPosition = (): THREE.Vector3 => this._sceneEntityGroup.position;
+
+  getRadius = () => this._radius;
+
+  getChildPosition = (child: PDSNode): number => {
+    const pos = this._children.indexOf(child);
+    return pos;
+  };
+
+  getChildren = (): PDSNode[] => this._children;
+
+  addToChildren = (child: PDSNode) => {
+    this._children.push(child);
   };
 }
--- src/PdsNodeNav/PdsNodeNavSceneManager.ts
+++ src/PdsNodeNav/PdsNodeNavSceneManager.ts
@@ -1,29 +1,132 @@
+import * as THREE from 'three';
 import { SceneManagerBase, ISceneManager } from '../ThreeJsScaffold/scene.manager';
 import { MiscHelpers } from './PdsNodeNavEntities/Helpers';
 import { DirectionalLight } from './PdsNodeNavEntities/DirectionalLight';
 import { PDSNode } from './PdsNodeNavEntities/PDSNode';
+import { distanceBetweenPoints } from 'src/ThreeJsScaffold/utils/threeGeometry';
 
 export class PDSNodeNavSceneManager extends SceneManagerBase implements ISceneManager {
+  private _raycaster = new THREE.Raycaster();
+  private _mouse = new THREE.Vector2();
+  private _zoomingObject: PDSNode | null = null;
+  private _isZooming = false;
+  private _radiusShrinkFactor = 0.3;
+
   constructor(container: HTMLElement) {
     super(container);
 
-    // Style canvas
+    this._orbitControls.enabled = false;
+
+    // Set up
     container.style.backgroundColor = 'rgba(0,0,0,1)';
+    this._camera.position.set(0, 0, 10);
+    this._updateCamera = this.updateCamera;
+
+    // Create nested PDSNodes
+    // this.allPDSNodes = {};
+
+    const rf = this._radiusShrinkFactor;
+
+    const lev000 = new PDSNode(this, null, 'NASA', 1);
+    const lev110 = new PDSNode(this, lev000, 'SBN', rf);
+    const lev120 = new PDSNode(this, lev000, 'Engineering', rf);
+    const lev130 = new PDSNode(this, lev000, 'Atmospheres', rf);
+    const lev140 = new PDSNode(this, lev000, 'Geosciences', rf);
+    const lev150 = new PDSNode(this, lev000, 'Cartography & Imaging', rf);
+    const lev160 = new PDSNode(this, lev000, 'Navigational & Ancillary Information', rf);
+    const lev170 = new PDSNode(this, lev000, 'Planetary Plasma Interactions', rf);
+    const lev180 = new PDSNode(this, lev000, 'Ring-Moon Systems', rf);
+    const lev111 = new PDSNode(this, lev110, 'Planetary Science Institute', rf * rf);
+    const lev112 = new PDSNode(this, lev110, 'Minor Planets Center', rf * rf);
+    const lev113 = new PDSNode(this, lev110, 'IAWN', rf * rf);
+    const lev114 = new PDSNode(this, lev110, 'CATCH', rf * rf);
+    const lev115 = new PDSNode(this, lev110, 'Legacy SBN', rf * rf);
 
     // Create entities
     this._sceneEntities = [
-      //
-      new DirectionalLight(this),
-      new MiscHelpers(this),
+      // new DirectionalLight(this),
+      // new MiscHelpers(this),
       // new SimpleLight(this),
-      new PDSNode(this, null, 1)
+
+      lev000,
+      lev110,
+      lev120,
+      lev130,
+      lev140,
+      lev150,
+      lev160,
+      lev170,
+      lev180,
+      lev111,
+      lev112,
+      lev113,
+      lev114,
+      lev115
     ];
 
-    // Set this as entities parent
+    // Load all entities
     this._sceneEntities.forEach(el => el.init());
+
+    // Set up raycasting
+    window.addEventListener('click', this.onMouseClick, false);
   }
+
+  onMouseClick = (event: MouseEvent) => {
+    // set up raycaster from mouse click
+    this._mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+    this._mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+    this._raycaster.setFromCamera(this._mouse, this._camera);
+    console.log('>>>', this._mouse.toArray());
+
+    // find all named meshes intersecting the picking ray
+    const allMeshes: any[] = [];
+    this._scene.children.forEach(el => el.children.forEach(el2 => allMeshes.push(el2)));
+    const intersectingMeshes = this._raycaster.intersectObjects(allMeshes);
+
+    // if a mesh is named, zoom the camera to it
+    const intersectingMeshesNames = intersectingMeshes.map(el => el.object.name).filter(el => !!el);
+    console.log(intersectingMeshesNames);
+    if (intersectingMeshesNames.length > 1) throw new Error('Nodes are overlapping somehow');
+    else if (intersectingMeshesNames.length === 1) {
+      const foundName = intersectingMeshesNames[0];
+      this.zoomToPDSNode(foundName);
+    }
+  };
 
   updateCamera = (time: number) => {
     //
+    if (!!this._zoomingObject) {
+      // const newPosition = this._zoomingMesh!.position + new THREE.Vector3();
+      const newPosition = this._zoomingObject!.getSceneEntityGroup().position.clone();
+      // this._camera.lookAt(newPosition);
+      newPosition.setZ(5 * this._zoomingObject.getRadius());
+      // console.log(newPosition.toArray());
+      const v = this._camera.position.lerp(newPosition, this._isZooming ? 0.1 : 1);
+      this._camera.position.set(v.x, v.y, v.z);
+      // console.log('x', v.x, v.y, v.z);
+
+      const dist = distanceBetweenPoints(this._camera.position, newPosition);
+
+      if (dist < 0.1) this._isZooming = false;
+    }
   };
+
+  zoomToPDSNode = (name: string) => {
+    console.log('>>>', name);
+    this._sceneEntities.forEach(sceneEntity => {
+      // sceneEntity.getSceneEntityGroup().children.forEach(object => {
+      const sceneEntityGroup = sceneEntity.getSceneEntityGroup();
+      if (sceneEntityGroup.name === name) {
+        // object-name match
+        // this._zoomel.position.clone();
+        console.log('Found Name: ', sceneEntityGroup.name, sceneEntityGroup.type);
+        if (sceneEntity instanceof PDSNode) {
+          this._zoomingObject = sceneEntity;
+          this._isZooming = true;
+        }
+      }
+    });
+  };
+
+  getRadiusShrinkFactor = () => this._radiusShrinkFactor;
 }
